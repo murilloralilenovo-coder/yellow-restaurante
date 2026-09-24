afe58a5e6d95df6a9788c51ae91f890df04266f3
MURILLO ALMEIDA RALILE <cg3047547@car.ifsp>
2026-09-24 16:55:09 -0300

    adicionando calculadora com java

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let prato={
     nome:"prato1",
     preco:15
 };
+let resultado = calcutotal(prato.preco, 0)
+let saida = document.querySelector("#total-pedido");
+saida.textContent = "total: "+resultado + " reais";
 function calcutotal(preco, quantidade){
     let total = preco * quantidade
     if(total >= 100){
@@ -9,5 +12,3 @@ function calcutotal(preco, quantidade){
     };
     return total
 }
-
-console.log(calcutotal(prato.preco, 7), "reais");
